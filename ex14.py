642595ab62b6912e834879b0d79729c6c752475e
fifteenzhang <799917669@qq.com>
2016-07-19 19:07:49 +0800

    Update ex14.py

--- ex14.py
+++ ex14.py
@@ -2,8 +2,9 @@ from sys import argv
 
 script, user_name = argv
 prompt = '> '
+#prompt为用户提示字符，可以方便的更改其他用户提示字符
 
-print "Hi %s, I'm the %s script." % (user_name, script)
+print "Hi %s, I'm the %s script." % (user_name, script)#注意是% (user_name, script)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
 likes = raw_input(prompt)
