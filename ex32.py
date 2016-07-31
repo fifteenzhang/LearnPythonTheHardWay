80076c14b6ed47c194750ff8bca0d622884ebaec
fifteenzhang <799917669@qq.com>
2016-07-31 20:29:55 +0800

    Update ex32.py

--- ex32.py
+++ ex32.py
@@ -18,7 +18,7 @@ for i in change:
 # we can also build lists, first start with an empty one
 elements = []
 
-# then use the range function to do 0 to 5 counts
+# then use the range function to do 0 to 5 counts（range(5)=range(0,6)=[0, 1, 2, 3, 4, 5]）
 for i in range(5):
     print "Adding %d to the list." % i
     # append is a function that lists understand
